b289f60d3a8baf12df105286fef1ff5804a0b9c1
Nael <nael.shuwaikh@gmail.com>
2022-10-18 18:59:25 +0200

    Initial Commit

---
+++ sideMenu2/sideMenu2App.swift
@@ -0,0 +1,17 @@
+//
+//  sideMenu2App.swift
+//  sideMenu2
+//
+//  Created by Nael Alshowaikh on 18.10.22.
+//
+
+import SwiftUI
+
+@main
+struct sideMenu2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
