98df83e42990271a801f45b3675fc81ea24c6cfa
Nael <nael.shuwaikh@gmail.com>
2022-10-19 19:18:29 +0200

    show from the other side

--- sideMenu2/ContentView.swift
+++ sideMenu2/ContentView.swift
@@ -18,21 +18,16 @@ struct ContentView: View {
                 Text("Hello, world!")
                     .padding()
 
-                VStack {
-                    Text("Contentview")
-                        .padding()
-                        .font(.title)
-                        .foregroundColor(.white)
 
-                }
 
                 GeometryReader { _ in
-                    HStack {
-                        Spacer()
+                    EmptyView()
+                    VStack(alignment: .leading) {
+                        //Spacer()
                         SideMenuView()
-                            .offset(x: showMenu ? 0 : UIScreen.main.bounds.width)
-                            .animation(.easeIn(duration: 0.3), value: showMenu)
-                    }
+                            .offset(x: showMenu ? 0 : UIScreen.main.bounds.width - 1000)
+                            
+                    }.animation(.easeInOut(duration: 0.3), value: showMenu)
                 }
                     .background(Color.black.opacity(showMenu ? 0.5 : 0))
 
@@ -41,22 +36,24 @@ struct ContentView: View {
                 .navigationTitle("side menu demo")
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
 
-                Button {
-                    print("Show menu")
-                    self.showMenu.toggle()
-                } label: {
+                    Button {
+                        print("Show menu")
+                        self.showMenu.toggle()
+                    } label: {
 
-                    if showMenu {
+                        if showMenu {
 
-                        Image(systemName: "xmark")
-                            .font(.title)
-                            .foregroundColor(.blue) }
-                    else {
+                            Image(systemName: "xmark")
+                                .font(.title)
+                                .foregroundColor(.blue) }
+                        else {
 
-                        Image(systemName: "text.justify")
-                            .font(.title)
-                            .foregroundColor(.blue) }
+                            Image(systemName: "text.justify")
+                                .font(.title)
+                                .foregroundColor(.blue) }
+                    }
                 }
 
             }
