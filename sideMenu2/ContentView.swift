34c0d8d0e669746ee03c097634adc83fd7ff9211
Nael <nael.shuwaikh@gmail.com>
2022-10-19 18:32:30 +0200

    finished side menu!!!!!

--- sideMenu2/ContentView.swift
+++ sideMenu2/ContentView.swift
@@ -8,16 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showMenu: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        NavigationView {
+            ZStack {
+                Color.mint.ignoresSafeArea(.all, edges: .all)
+                Text("Hello, world!")
+                    .padding()
+
+                VStack {
+                    Text("Contentview")
+                        .padding()
+                        .font(.title)
+                        .foregroundColor(.white)
+
+                }
+
+                GeometryReader { _ in
+                    HStack {
+                        Spacer()
+                        SideMenuView()
+                            .offset(x: showMenu ? 0 : UIScreen.main.bounds.width)
+                            .animation(.easeIn(duration: 0.3), value: showMenu)
+                    }
+                }
+                    .background(Color.black.opacity(showMenu ? 0.5 : 0))
+
+            }
+
+                .navigationTitle("side menu demo")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+
+                Button {
+                    print("Show menu")
+                    self.showMenu.toggle()
+                } label: {
+
+                    if showMenu {
+
+                        Image(systemName: "xmark")
+                            .font(.title)
+                            .foregroundColor(.blue) }
+                    else {
+
+                        Image(systemName: "text.justify")
+                            .font(.title)
+                            .foregroundColor(.blue) }
+                }
+
+            }
+
+
+
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
